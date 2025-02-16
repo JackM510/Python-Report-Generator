ef9f994f0eb18e580e84c7dbacc6781247d7cbcc
JackM510 <37175625+JackM510@users.noreply.github.com>
2025-02-17 08:15:32 +1100

    Update logged_in_users.py
    Simplified some of the comments in the script to improve readability.

--- logged_in_users.py
+++ logged_in_users.py
@@ -1,6 +1,4 @@
-#the event class keeps record of users logging in/out of a specific machine.
-#an event is comprised of an event date, the type of event (log in OR log out),
-#the machine the event occured on, and the user which triggered the event.
+# The event class keeps record of users logging in/out of a specific machine
 class Event:
     def __init__(self, event_date, event_type, machine_name, user):
         self.date = event_date
@@ -8,29 +6,25 @@ class Event:
         self.machine = machine_name
         self.user = user
 
-#this function returns the date of each event and is used in current_users function.
+# Returns the date of each event and is used in current_users() function
 def get_event_date(event):
     return event.date
 
-#this function sorts each Event in the events List by the event date using get_event_date().
-#an empty Dictionary is instantiated and uses conditional statements to add key-value pairs:
-#each machine is added to the machines Dictionary as a key.
-#each user that is logged into a machine is stored as a value in an empty set() element.
-#each user that logs out of a machine is removed from the key-value pair for that particular machine.
+# Sorts each Event in the events List by the event date using get_event_date()
+# Returns a Dictionary of machines that have recorded an event where a user has logged in
 def current_users(events):
-    events.sort(key=get_event_date) #sort each Event in the events List by date.
-    machines = {} #intialise an empty Dictionary.
+    events.sort(key=get_event_date)
+    machines = {} # Intialise an empty Dictionary
     for event in events:
-        if event.machine not in machines: #if a machine in the events List is not in the machines Dictionary:
-            machines[event.machine] = set() #add the machine to the machines Dictionary and create an empty set() which will store a set of users logged into that machine.
-        if event.type == "login": #if an event type specifies a login:
-            machines[event.machine].add(event.user) #add the user to the machines Dictionary under the machine they are logged into.
-        elif event.type == "logout": #if an event type specifies a logout:
-            machines[event.machine].remove(event.user) #remove the user from the machines Dictionary under the machine they are logging out of.
+        if event.machine not in machines:
+            machines[event.machine] = set()
+        if event.type == "login":
+            machines[event.machine].add(event.user) # Add the user to Dictionary if 'login'
+        elif event.type == "logout":
+            machines[event.machine].remove(event.user) # Remove the user from Dictionary if 'logout'
     return machines
 
-#this function generates a report of users logged into a machine.
-#this function will automatically print out the report results to the console.
+# Generates a report of users logged into a machine and returns the results to the terminal
 def generate_report(machines):
     print("Below is a list of users that are logged into a machine:")
     for machine, users in machines.items():
@@ -38,8 +32,7 @@ def generate_report(machines):
             user_list = ", ".join(users)
             print("{}: {}".format(machine, user_list))
 
-#the events variable below is a List which contains several Event objects.
-#each event holds event data for a user logging in/out of a machine.
+# Holds a list of Events
 events = [
     Event('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
     Event('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
@@ -51,9 +44,8 @@ events = [
     Event('2020-01-23 14:24:49', 'login', 'cloud-vm-ps01', 'chris'),
 ]
 
-#create a Dictionary of users based on each Event listed in the events List.
+# Create a Dictionary of users that are logged in using the current_users() function
 users = current_users(events)
 
-#use the generate_report function and pass the list of users to return only
-#a list of users that are logged into a machine.
+# Pass the Dictionary as a parameter and generate a report of users that are logged in
 generate_report(users)
